fbeb19e8682eeedc1bf0270c6c3fd92a1f7bf50e
MikaYuoadas <mikayuoadas@gmail.com>
2011-06-05 18:14:54 +0200

    added advance function so Projectiles can move.

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -1,10 +1,24 @@
+#include <cmath>
+
 #include "Projectile.h"
 #include "define.h"
 
-Projectile::Projectile(QPointF start, QPointF dest, double init_speed) :
-    destination(dest), speed(init_speed)
+Projectile::Projectile(QPointF start, QPointF destination, double init_speed, double damage) :
+    dest(destination), speed(init_speed), dmg(damage), travelled(0)
 {
+    maxrange = sqrt(pow(2, dest.x() - start.x()) + pow(2, dest.y() - start.y()));
     setPos(start);
+}
+
+void Projectile::advance(int step)
+{
+    if (!step)
+        return;
+    double hyp = sqrt(pow(2, dest.x() - pos().x()) + pow(2, dest.y() - pos().y()));
+    double rspeed = speed * BASE_SPEED;
+    moveBy((dest.x() - pos().x()) / hyp * rspeed, (dest.y() - pos().y()) / hyp * rspeed);
+    travelled += rspeed;
+    // if travelled > maxrange explode
 }
 
 QRectF Projectile::boundingRect() const
@@ -12,8 +26,8 @@ QRectF Projectile::boundingRect() const
     return QRectF(-PROJECTILE_SIZE / 2, -PROJECTILE_SIZE / 2, PROJECTILE_SIZE, PROJECTILE_SIZE);
 }
 
-void Projectile::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget * widget)
+void Projectile::paint(QPainter *painter, QColor color)
 {
-    painter->setBrush(Qt::black);
+    painter->setBrush(color);
     painter->drawEllipse(QRect(-PROJECTILE_SIZE / 2, - PROJECTILE_SIZE / 2, PROJECTILE_SIZE, PROJECTILE_SIZE));
 }
--- src/Projectile.h
+++ src/Projectile.h
@@ -10,13 +10,19 @@
 
 class Projectile : public QGraphicsItem
 {
+    protected:
+        void advance(int step);
+
     private:
-        QPointF destination;
+        QPointF dest;
         double speed;
+        double dmg;
+        double maxrange;
+        double travelled;
 
     public:
-        Projectile(QPointF start, QPointF dest, double init_speed);
+        Projectile(QPointF start, QPointF destination, double init_speed, double damage);
         QRectF boundingRect() const;
-        void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+        void paint(QPainter *painter, QColor color);
 };
 #endif
