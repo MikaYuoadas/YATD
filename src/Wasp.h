fe04a99619a3d75e6e8da6a4d5ad8ccdc63fa3b5
MikaYuoadas <mikayuoadas@gmail.com>
2011-06-08 23:34:13 +0200

    Added doxygen documentation and comments for Wasp.h .

--- src/Wasp.h
+++ src/Wasp.h
@@ -5,14 +5,34 @@
 
 #include "Bug.h"
 
+/**
+ * Classe représentant les guêpes.
+ * Définie les caractéristiques propres aux guêpes.
+ */
 class Wasp : public Bug
 {
     private:
-        QImage * image[2];
+        QImage * image[2]; /**< Cache d'images. Les images de la guêpes redimensionnées.*/
 
     public:
+        /**
+         * Constructeur de Wasp.
+         * Appelle le constructeur de Bug avec les paramètres spécifiques aux guêpes.
+         * @param x L'abscisse du départ des insectes.
+         * @param y L'ordonnée du départ des insectes.
+         * @param s La taille de la guêpe.
+         * @param start_angle l'angle de départ des insectes.
+         */
         Wasp(double x, double y, double s, double start_angle);
+        /**
+         * Destructeur de Wasp.
+         * Désalloue la mémoire des images.
+         */
         ~Wasp();
+        /**
+         * Surcharge de la méthode virtuelle pure paint() héritée de QGraphicsObject.
+         * Est appelé automatiquement par Qt pour dessiner la guêpe.
+         */
         void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 };
 #endif
