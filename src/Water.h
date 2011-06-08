28a5e236378495910f5b9675928dfcdc9deb36bd
MikaYuoadas <mikayuoadas@gmail.com>
2011-06-08 23:38:19 +0200

    Added doxygen documentation and comments for Water.h .

--- src/Water.h
+++ src/Water.h
@@ -3,10 +3,25 @@
 
 #include "Projectile.h"
 
+/**
+ * Classe représentant les projectiles du pistolet à eau.
+ * Classe représentant les projectiles du pistolet à eau, rien de spéciale.
+ */
 class Water : public Projectile
 {
     public:
+        /**
+         * Constructeur de Ware.
+         * Appelle le constructeur de Projectile avec les caractéristiques du pistolet à eau.
+         * @param start Position de départ.
+         * @param target Pointeur vers l'insecte ciblé.
+         * @param lvl Le niveau du projectile.
+         */
         Water(QPointF start, Bug * target, short int lvl);
+        /**
+         * Surcharge de la méthode virtuelle pure paint() héritée de QGraphicsObject.
+         * Appelé automatiquement par Qt pour redessiner l'objet.
+         */
         void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 };
 #endif
